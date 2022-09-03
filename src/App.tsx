fd3bb8bc41f93e35380120aa232b066b1d243eee
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2022-09-03 22:28:37 +0900

    TypeScript, StyledComponent Setting

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+function App() {
+
+  return (
+    <div>
+      <Container>
+        <H1>Hello World</H1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
